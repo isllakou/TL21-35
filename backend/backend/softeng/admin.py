d155c4725b61be36b9f0f4875d7923a2f2bf4ba3
isllakou <el17825@mail.ntua.gr>
2021-12-13 12:08:10 +0200

    Add Operator and Invoice Model

--- backend/backend/softeng/admin.py
+++ backend/backend/softeng/admin.py
@@ -4,10 +4,21 @@ Eisagei ola ta model sto arxeio admin
 gia na mhn ta eisagoyme ena ena"""
 from .models import * 
 
+
 class ChargeAdmin(admin.ModelAdmin):
     list_display = ('tagID', 'operatorDebited', 
         'dateOfCharge', 'amount', 'vehicleID')
 
+class OperatorAdmin(admin.ModelAdmin):
+    list_display = ('operatorID', 'username', 'password', 'status')
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoiceID', 'operatorID', 'amount', 'startingDate', 'column')
+
+
 # Register your models here.
 
 admin.site.register(Charge, ChargeAdmin)
+admin.site.register(Operator, OperatorAdmin)
+admin.site.register(Invoice, InvoiceAdmin)
+
